dca641a1de98476f8d477546da492ad62f1f48c3
hl3470 <hl3470@naver.com>
2024-02-19 22:17:05 +0900

    init commit

---
+++ backend/src/test/java/hongblinddate/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hongblinddate.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
