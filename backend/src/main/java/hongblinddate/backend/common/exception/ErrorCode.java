6287299c45f3adb181b58fee32cf01c63a8ece32
hl3470 <hl3470@naver.com>
2024-02-21 15:45:51 +0900

    feat: ErrorCode에 MemberNotFoundException추가

--- backend/src/main/java/hongblinddate/backend/common/exception/ErrorCode.java
+++ backend/src/main/java/hongblinddate/backend/common/exception/ErrorCode.java
@@ -8,8 +8,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public enum ErrorCode {
-    AccountNotFoundException(HttpStatus.NOT_FOUND, "ACCOUNT_NOT_FOUND_00!" , "계정이 존재하지 않습니다."),
-    EmailNotFoundException(HttpStatus.NOT_FOUND, "EMAIL_NOT_FOUND_00!" , "이메일이 존재하지 않습니다.")
+    AccountNotFoundException(HttpStatus.NOT_FOUND, "ACCOUNT_NOT_FOUND_001" , "계정이 존재하지 않습니다."),
+    EmailNotFoundException(HttpStatus.NOT_FOUND, "EMAIL_NOT_FOUND_001" , "이메일이 존재하지 않습니다."),
+    MemberNotFoundException(HttpStatus.NOT_FOUND, "MEMBER_NOT_FOUND_001", "해당하는 멤버가 없습니다."),
     ;
 
     private final HttpStatus httpStatus;
