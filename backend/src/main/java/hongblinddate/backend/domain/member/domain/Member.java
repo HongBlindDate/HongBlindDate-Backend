f0d4188864275fe3d914d4260f2790569d4a7428
hl3470 <hl3470@naver.com>
2024-03-01 23:41:13 +0900

    feat: Member에 refreshToken 삭제 로직 추가

--- backend/src/main/java/hongblinddate/backend/domain/member/domain/Member.java
+++ backend/src/main/java/hongblinddate/backend/domain/member/domain/Member.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Entity
@@ -13,56 +14,59 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
-    @Id
-    @Column(name = "member_id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@Column(name = "member_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @NotBlank
-    @Column(unique = true)
-    private String account;
+	@NotBlank
+	@Column(unique = true)
+	private String account;
 
-    @NotBlank
-    @Size(min = 6)
-    private String password;
+	@NotBlank
+	@Size(min = 6)
+	private String password;
 
-    @NotBlank
-    @Email
-    @Column(unique = true)
-    private String email;
+	@NotBlank
+	@Email
+	@Column(unique = true)
+	private String email;
 
-    @NotBlank
-    private String nickName;
+	@NotBlank
+	private String nickName;
 
-    @NotNull
-    @Enumerated(EnumType.STRING)
-    private Grade grade;
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	private Grade grade;
 
-    private String refreshToken; // 리프레시 토큰
+	private String refreshToken; // 리프레시 토큰
 
-    public void passwordEncode(PasswordEncoder passwordEncoder) {
-        this.password = passwordEncoder.encode(this.password);
-    }
+	public void passwordEncode(PasswordEncoder passwordEncoder) {
+		this.password = passwordEncoder.encode(this.password);
+	}
 
-    public void updateRefreshToken(String updateRefreshToken) {
-        this.refreshToken = updateRefreshToken;
-    }
+	public void updateRefreshToken(String updateRefreshToken) {
+		this.refreshToken = updateRefreshToken;
+	}
 
-    private Member(String account, String password, String email, String nickName, Grade grade) {
-        this.account = account;
-        this.password = password;
-        this.email = email;
-        this.nickName = nickName;
-        this.grade = grade;
-    }
+	private Member(String account, String password, String email, String nickName, Grade grade) {
+		this.account = account;
+		this.password = password;
+		this.email = email;
+		this.nickName = nickName;
+		this.grade = grade;
+	}
 
-    public static Member create(JoinRequest joinRequest, Grade grade) {
-        return new Member(joinRequest.getAccount(),
-                joinRequest.getPassword(),
-                joinRequest.getEmail(),
-                joinRequest.getNickName(),
-                grade);
-    }
+	public static Member create(JoinRequest joinRequest, Grade grade) {
+		return new Member(joinRequest.getAccount(),
+			joinRequest.getPassword(),
+			joinRequest.getEmail(),
+			joinRequest.getNickName(),
+			grade);
+	}
 
+	public void deleteRefreshToken() {
+		this.refreshToken = null;
+	}
 }
 
